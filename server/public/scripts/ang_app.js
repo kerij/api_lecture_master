73414198269208f12042a245318d2a09c58e5bdc
kerij <keriajackson@gmail.com>
2016-08-26 16:04:28 -0500

    api and routing works

--- server/public/scripts/ang_app.js
+++ server/public/scripts/ang_app.js
@@ -1,7 +1,49 @@
-var myApp = angular.module('myApp', []);
+var myApp = angular.module('myApp', ['ngRoute']);
 
-myApp.controller("petController", ["$scope", "$http", function($scope, $http) {
-  var key = 'b900e0d5e332753a460a64eaa8de00fd';
+myApp.config(['$routeProvider', function($routeProvider) {
+  $routeProvider
+    .when('/cat', {
+      templateUrl: '/views/partials/cat.html',
+      controller: "catController"
+    })
+    .when('/barnyard', {
+      templateUrl: '/views/partials/barnyard.html',
+      controller: "barnyardController"
+    })
+    .when('/smallfurry', {
+      templateUrl: '/views/partials/smallfurry.html',
+      controller: "smallfurryController"
+    })
+    .otherwise({
+      redirectTo: '/index.html'
+    })
+}]);
+
+myApp.controller("catController", ["$scope", "$http", function($scope, $http) {
+  var key = 'beb24f67911c3af4d528df369362a516';
+  var baseURL = 'http://api.petfinder.com/';
+
+  $scope.getRandomPet = function() {
+    var query = baseURL + 'pet.getRandom';
+    query += '?key=' + key;
+    query += '&animal=cat';
+    query += '&output=basic';
+    query += '&format=json';
+
+    console.log('query: ', query);
+
+    var request = encodeURI(query) + '&callback=JSON_CALLBACK';
+
+    $http.jsonp(request).then(function(response) {
+      $scope.pet = response.data.petfinder.pet;
+
+    });
+  }
+  $scope.getRandomPet();
+}]);
+
+myApp.controller("barnyardController", ["$scope", "$http", function($scope, $http) {
+  var key = 'beb24f67911c3af4d528df369362a516';
   var baseURL = 'http://api.petfinder.com/';
 
   $scope.getRandomPet = function() {
@@ -19,10 +61,29 @@ myApp.controller("petController", ["$scope", "$http", function($scope, $http) {
       $scope.pet = response.data.petfinder.pet;
 
     });
-
-
-
-
   }
+  $scope.getRandomPet();
+}]);
 
+myApp.controller("smallfurryController", ["$scope", "$http", function($scope, $http) {
+  var key = 'beb24f67911c3af4d528df369362a516';
+  var baseURL = 'http://api.petfinder.com/';
+
+  $scope.getRandomPet = function() {
+    var query = baseURL + 'pet.getRandom';
+    query += '?key=' + key;
+    query += '&animal=smallfurry';
+    query += '&output=basic';
+    query += '&format=json';
+
+    console.log('query: ', query);
+
+    var request = encodeURI(query) + '&callback=JSON_CALLBACK';
+
+    $http.jsonp(request).then(function(response) {
+      $scope.pet = response.data.petfinder.pet;
+
+    });
+  }
+  $scope.getRandomPet();
 }]);
